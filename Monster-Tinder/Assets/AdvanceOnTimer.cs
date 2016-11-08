1eeee7903b330324bc6e14b9719889419d0d1a91
phelow <wpheloung@gmail.com>
2016-11-07 21:26:20 -0500

    epic intro animation

---
+++ Monster-Tinder/Assets/AdvanceOnTimer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class AdvanceOnTimer : MonoBehaviour {
+    public float m_curTime = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_curTime += Time.deltaTime;
+
+        if(m_curTime > 45.0f)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+
+    }
+}
